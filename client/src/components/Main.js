4771ab955850392a597f454e670f1f4a23c32e49
ttatsato <t-satoh@alby.co.jp>
2022-08-30 22:08:14 +0900

    fc

---
+++ client/src/components/Main.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Main = () => {
+    return (
+        <div className="mainContainer">
+            <div className="cryptContainer">
+                <h1 className="title">Crypto Card</h1>
+                <button type="button">
+                    <p className="buttonText">ウォレット連携</p>
+                </button>
+            </div>
+            <div className="inputContainer">
+                <input type="text" placeholder={"アドレス"}/>
+                <input type="number"  placeholder={"通貨（ETH)"} name={"amount"} step={0.0001}/>
+                <button type={"button"}>送信</button>
+            </div>
+        </div>
+    )
+
+}
+export default Main
+
